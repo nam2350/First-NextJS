d145899f6cbfd1ab9b07efddc63c4b07d971e344
nam2350 <nam2350@gmail.com>
2022-10-19 18:32:20 +0900

    windows setting

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import styles from "./NavBar.module.css";
 
 export default function NavBar() {
   const router = useRouter();
